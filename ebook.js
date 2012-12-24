e61cb5e4d425e929ce398948d874685b4a783f54
Igonina Galina <igonina2101@gmail.com>
2012-12-24 23:57:01 +1100

    Электронная книга сделана, все flash добавлены и добавлены дополнительные презентации

--- ebook.js
+++ ebook.js
@@ -21,4 +21,216 @@ $(document).ready(function(){
 									"</object>");
 									
 	});
+	$('#lection3').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №3. Система автоматизированного проектирования.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №3. Система автоматизированного проектирования.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection4').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №4. Преобразования в двухмерном пространстве.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №4. Преобразования в двухмерном пространстве.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection5').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №5. Алгоритмические основы компьютерной графики.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №5. Алгоритмические основы компьютерной графики.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection6').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №6. Введение в OpenGL.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №6. Введение в OpenGL.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection7').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №7. Работа с OpenGL.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №7. Работа с OpenGL.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection8').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №8. Модельные и перспективные преобразования в OpenGL.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №8. Модельные и перспективные преобразования в OpenGL.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection9').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №9. Текстурирование с использованием OpenGL.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №9. Текстурирование с использованием OpenGL.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection10').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №10. Освещение в OpenGL.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №10. Освещение в OpenGL.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection11').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №11. Создание электронных учебных материалов.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №11. Создание электронных учебных материалов.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection12').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №12. Смешивание, сглаживание и туман в OpenGL.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №12. Смешивание, сглаживание и туман в OpenGL.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection13').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №13. Стандарты компьютерной графики.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №13. Стандарты компьютерной графики.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection14').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №14. СРЕДСТВА И ПРИЕМЫ КОМПОЗИЦИИ.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №14. СРЕДСТВА И ПРИЕМЫ КОМПОЗИЦИИ.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection15').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №15. ТЕОРИЯ ЦВЕТА.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №15. ТЕОРИЯ ЦВЕТА.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection16').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"ОПЕРАЦИИ С ПИКСЕЛЯМИ.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/ОПЕРАЦИИ С ПИКСЕЛЯМИ.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection17').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"ОПТИЧЕСКИЕ ИЛЛЮЗИИ.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/ОПТИЧЕСКИЕ ИЛЛЮЗИИ.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection18').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Контейнер OpenGL.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Контейнер OpenGL.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection19').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"ДИЗАЙН презентаций.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/ДИЗАЙН презентаций.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection20').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"САМОСТОЯТЕЛЬНАЯ РАБОТА №1.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/САМОСТОЯТЕЛЬНАЯ РАБОТА №1.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	$('#lection21').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Тест по дисциплине компьютерная графика по теме OpenGL.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Тест по дисциплине компьютерная графика по теме OpenGL.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+	
+
 }); 
+
