dcacd82cbfb000c2fe210cd7bd168f3a598c945b
Igonina Galina <igonina2101@gmail.com>
2012-12-24 22:53:09 +1100

    Добавила функции qQuery для вывода на экран flash

---
+++ ebook.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('#lection1').click(function() {									
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №1 Введение в компьютерную графику.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №1 Введение в компьютерную графику.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+	});
+	
+	$('#lection2').click(function() {											
+									$("#lection").html("<object id=\"presentation\" width=\"722\" height=\"577\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" align=\"right\">" +
+										"<param name=\"allowScriptAccess\" value=\"sameDomain\" />"+
+										"<param name=\"movie\" value=\"Лекция №2. Основы компьютерной графики.swf\" />"+
+										"<param name=\"quality\" value=\"high\" />"+
+										"<param name=\"bgcolor\" value=\"#ffffff\" />"+
+										"<param name=\"allowFullScreen\" value=\"true\" />"+
+											"<embed src=\"content/Лекция №2. Основы компьютерной графики.swf\" quality=\"high\" background-color=\"#ffffff\" width=\"722\" height=\"577\" name=\"presentation\" align=\"right\" allowScriptAccess=\"sameDomain\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.adobe.com/go/getflashplayer\" allowFullScreen=\"true\" />"+
+									"</object>");
+									
+	});
+}); 
